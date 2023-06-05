a8f4687f283b1d2658db5fb003c37faf2f4b1dfc
Jo-Shino <joe000042@gmail.com>
2023-06-05 15:46:08 +0900

    WavePortal.solの作成

---
+++ packages/contract/contracts/WavePortal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+  constructor() {
+    console.log("Yo yo, I am a contract and I am smart");
+  }
+}
